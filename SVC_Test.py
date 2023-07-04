253d2c7a898b6772ba0720745482fee02f5cc052
Ilyas <adakoeyilyas@gmail.com>
2023-07-04 13:24:33 +0200

    SVM für den isentropen Wirkungsgrad, unsicher, ob die Scale Sachen gemacht werden müssen

--- SVC_Test.py
+++ SVC_Test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 df = pd.read_excel("dummy3.xlsx")  # Einlesen der Daten
 
@@ -11,11 +12,15 @@ df = pd.read_excel("dummy3.xlsx")  # Einlesen der Daten
 #df_sc = scale.fit_transform(df)
 #df_sc = pd.DataFrame(df_sc,columns=df.columns)
 
-y = df['EtaS']
+y = df['EtaS'] # Output ''
 X = df.drop('EtaS',axis=1)
 
-Xtrain,XTest,ytrain,ytest = train_test_split(X,y,test_size=0.7,random_state=42)
+Xtrain,Xtest,ytrain,ytest = train_test_split(X,y,test_size=0.7,random_state=42)
 
 SVM_regression = SVR()
 SVM_regression.fit(Xtrain,ytrain)
 
+y_hat = SVM_regression.predict(Xtest)
+
+print(mean_squared_error(ytest, y_hat))
+print(mean_absolute_error(ytest, y_hat))
