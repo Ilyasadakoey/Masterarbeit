9bcaa43d5107fcefe1c20926d29a6d3ca076d2a5
Ilyas <adakoeyilyas@gmail.com>
2023-07-19 12:03:45 +0200

    Angepasst, sodass die Drücke in einer txt. gespeichert werden

--- find_psat.py
+++ find_psat.py
@@ -8,20 +8,14 @@ x1 = df["x1"]
 x2 = df["x2"]
 x3 = df["x3"]
 
-
-
 Tin = 278.15
 Tsat = Tin - 5
 fluid = "Isobutane*Propane*Propylene"
 
-
-for (a,b,c) in zip(x1,x2,x3):
-    evap = fprop.T_prop_sat(Tsat, fluid, composition=[a,b,c], option=1)
-    psat = evap[1,1]
+for (a, b, c) in zip(x1, x2, x3):
+    evap = fprop.T_prop_sat(Tsat, fluid, composition=[a, b, c], option=1)
+    psat = evap[1, 1]
     ptxt = str(psat)
-    with open ('data.txt','a') as f:
+    with open('data.txt', 'a') as f:
         f.write('\n')
         f.write(ptxt)
-
-
-
