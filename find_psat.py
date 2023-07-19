8e24ec3e5f19ea1dd556e6a7c0f3f32c52353412
Ilyas <adakoeyilyas@gmail.com>
2023-07-19 10:40:55 +0200

    Bei vorgegebener Eintrittstemperatur wird der Eintrittsdruck gesucht (10K überhitzt)

---
+++ find_psat.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import fluid_properties_rp as fprop
+
+
+x1 = [0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5]
+x2 = [0.1,0.12,0.14,0.16,0.18,0.2,0.22,0.24,0.26]
+
+x3 = 1 - np.array(x2)-np.array(x1)
+
+Tin = 308.15
+Tsat = Tin - 10
+fluid = "Isobutane*Propane*Propylene"
+
+
+for (a,b,c) in zip(x1,x2,x3):
+    evap = fprop.T_prop_sat(Tsat, fluid, composition=[a,b,c], option=1)
+    print(a,b,c)
+    print(evap[1,1])
+
+
+
