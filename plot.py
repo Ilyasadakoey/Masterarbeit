bdbad9068d96882f21864cd63d7b6237935f9e47
Ilyasadakoey <adakoeyilyas@gmail.com>
2023-08-21 13:26:38 +0200

    aktuell

--- plot.py
+++ plot.py
@@ -2,24 +2,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_excel("Isobutane_Propan_EtaS.xlsx")
+df = pd.read_excel("Datensatz.xlsx")
 
 
 
-x = df['p2/p1']
-y = df['EtaS']
+x = df['T_e']
+y = df['p_e']
 Z = df ['xa']
 
+
+
+
+
 # Streudiagramm mit Farbskala erstellen
-scatter = plt.scatter(x, y, c=Z, cmap='cividis',s=10)  # 'viridis' ist eine Farbskala, Sie können eine andere auswählen
-plt.colorbar(scatter, label='Werte der Farbskala')
+
+scatter = plt.scatter(x, y, c=Z, cmap='cividis',s=10)
+colorbar = plt.colorbar(scatter, label='Molenbruch Isobutan / -')
+
+colorbar.set_label('Molenbruch Isobutan / -', fontsize = 10)
+
+
+plt.grid(True)
+
+
+
+
+colorbar.ax.yaxis.set_label_coords(4, 0.5)
+
+
+plt.xlim(0)
+plt.ylim(0)
 
 # Achsenbeschriftungen
-plt.xlabel('Variable X')
-plt.ylabel('Variable Y')
+plt.xlabel('Eintrittstemperatur / K',fontsize = 10)
+plt.ylabel('Eintrittsdruck / kPa', fontsize = 10)
 
-# Titel hinzufügen
-plt.title('Streudiagramm mit Farbskala')
+plt.xticks(fontsize = 10)
+plt.yticks(fontsize = 10)
+
+colorbar.ax.tick_params(labelsize = 10)
+
+
+
+plt.gca().spines['top'].set_linewidth(2)
+plt.gca().spines['right'].set_linewidth(2)
+plt.gca().spines['bottom'].set_linewidth(2)
+plt.gca().spines['left'].set_linewidth(2)
 
 # Diagramm anzeigen
-plt.show()
+
+
+
+save_path = 'C:\\Users\\ilyas\\OneDrive\\Desktop\\'
+plt.savefig(save_path+'pin_vs_Tin_xa',dpi=500)
+
+
+#plt.tight_layout()
+#plt.show()
