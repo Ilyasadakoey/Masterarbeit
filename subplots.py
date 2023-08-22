58d75843e687048a9149517fd597b4d585cd1a8e
Ilyasadakoey <adakoeyilyas@gmail.com>
2023-08-22 13:59:16 +0200

    subplots

--- subplots.py
+++ subplots.py
@@ -12,33 +12,45 @@ y = df['p_e']
 Z1 = df ['xa']
 Z2 = df['dT']
 
-fig, (ax1,ax2) = plt.subplots(1,2,figsize=(8,6))
+fig, (ax1,ax2) = plt.subplots(1,2,figsize=(8.27,11.69))
 
 im1 = ax1.scatter(x,y,c=Z1,cmap='viridis')
-ax1.set_xlabel('Eintrittstemperatur / K',fontsize = 10)
-ax1.set_ylabel('Eintrittsdruck / kPa', fontsize = 10)
+ax1.set_xlabel('Eintrittstemperatur / K',fontsize = 15)
+ax1.set_ylabel('Eintrittsdruck / kPa', fontsize = 15)
 
 
-cbar1 = plt.colorbar(im1, ax=ax1)
-cbar1.set_label('Molenbruch Isobutan / -',fontsize = 10)
-#cbar1.ax.yaxis.set_label_position('right')
-ax1.tick_params(axis='both', which='major', labelsize=12)
-cbar1.ax.tick_params(labelsize=12)
+
+divider1 = make_axes_locatable(ax1)
+cax1 = divider1.append_axes("right",size="5%",pad=0.05)
+cbar1 = plt.colorbar(im1, cax=cax1)
+cbar1.set_label('Molenbruch Isobutan / -',fontsize = 15)
+ax1.tick_params(axis='both', which='major', labelsize=15)
+cbar1.ax.tick_params(labelsize=15)
+
+ax1.set_xlim(0,400)
+ax1.set_ylim(0)
+ax1.grid(True)
 
 im2 = ax2.scatter(x,y,c=Z2,cmap = 'viridis')
-ax2.set_xlabel('Eintrittstemperatur / K',fontsize = 10)
-ax2.set_ylabel('Eintrittsdruck / kPa', fontsize = 10)
-ax2.tick_params(axis='both', which='major', labelsize=12)
+ax2.set_xlabel('Eintrittstemperatur / K',fontsize = 15)
+ax2.set_ylabel('Eintrittsdruck / kPa', fontsize = 15)
+ax2.tick_params(axis='both', which='major', labelsize=15)
 
 
 
-cbar2 = plt.colorbar(im1, ax=ax1)
-cbar2.set_label('Überhitzung / K',fontsize = 10)
-#cbar2.ax.yaxis.set_label_position('right')
-cbar2.ax.tick_params(labelsize=12)
+divider2 = make_axes_locatable(ax2)
+cax2 = divider2.append_axes("right",size="5%",pad=0.05)
+cbar2 = plt.colorbar(im2, cax=cax2)
+cbar2.set_label('Überhitzung / K',fontsize = 15)
+cbar2.ax.tick_params(labelsize=15)
+ax2.set_xlim(0,400)
+ax2.set_ylim(0)
+ax2.grid(True)
 
 
 plt.tight_layout()
+
+
 
 plt.show()
 
