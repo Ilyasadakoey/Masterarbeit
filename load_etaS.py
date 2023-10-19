78cdb07e14fb0cbd5f8829180e71938834474b2f
Ilyasadakoey <adakoeyilyas@gmail.com>
2023-10-19 12:57:17 +0200

    load and use the ANN for etaS

--- load_etaS.py
+++ load_etaS.py
@@ -8,13 +8,15 @@ from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_excel("binarymixture2.xlsx")
 
-x = df.iloc[:, [0, 1, 2]]
+x = df.iloc[:, [0, 1, 2,3,4]]
 y = df['etaS']
+
+m = df['xa']
 
 scaler = MinMaxScaler()
 X = scaler.fit_transform(x)
 
-loaded_model = jb.load('EtaS_MLP_sens.pkl')
+loaded_model = jb.load('EtaS_MLP_alle.pkl')
 
 predictions = loaded_model.predict(X)
 
@@ -30,10 +32,26 @@ print(mean_absolute_error(y, predictions))
 print(r2_score(y, predictions))
 print(mean)
 
+Intervall_start = 0.50
+Intervall_ende = 0.70
+
+# Punkte im Intervall filtern
+in_interval = (m >= Intervall_start) & (m <= Intervall_ende)
+
+# Punkte außerhalb des Intervalls
+out_of_interval = ~in_interval
+
+# Punkte innerhalb des Intervalls in rot plotten
+plt.scatter(y[in_interval], predictions[in_interval], c='blue',s=5, label='Innerhalb Intervall')
+
+# Punkte außerhalb des Intervalls in schwarz plotten
+plt.scatter(y[out_of_interval], predictions[out_of_interval], c='black',s=5, label='Außerhalb Intervall')
+
+
 plt.rcParams["font.family"] = "Arial"
 plt.xlabel('Wahrer Wert / -', fontsize=11)
 plt.ylabel('Vorhergesagter Wert / -', fontsize=11)
-#plt.title('', fontsize=11)
+plt.title('Isentroper Wirkungsgrad für binäres Gemisch', fontsize=11)
 plt.xticks(fontsize=11)
 plt.yticks(fontsize=11)
 plt.gca().spines['top'].set_linewidth(2)
@@ -41,7 +59,7 @@ plt.gca().spines['right'].set_linewidth(2)
 plt.gca().spines['bottom'].set_linewidth(2)
 plt.gca().spines['left'].set_linewidth(2)
 
-plt.scatter(y, predictions, s=5)
+
 plt.plot([min(y), max(y)], [min(predictions), max(predictions)], linestyle='--', color='red', label='1:1-Linie')
 
 save_path = 'C:\\Users\\ilyas\\OneDrive\\Desktop\\'
